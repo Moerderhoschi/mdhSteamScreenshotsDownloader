5205760c9dfa76818c1f17e8158705ec7afc3b05
Moerderhoschi <163730007+Moerderhoschi@users.noreply.github.com>
2025-05-26 09:32:11 +0200

    Add files via upload

--- mdhSteamScreenshotsDownloader/mdhSteamScreenshotsDownloader.bat
+++ mdhSteamScreenshotsDownloader/mdhSteamScreenshotsDownloader.bat
@@ -1,7 +1,7 @@
 @echo off
 goto oStart
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-// mdhSteamScreenshotsDownloader(by Moerderhoschi) - v2025-05-25
+// mdhSteamScreenshotsDownloader(by Moerderhoschi) v2025-05-26
 // github: https://github.com/Moerderhoschi/mdhSteamScreenshotsDownloader
 // a set of Javascript and Powershell code to download screenshots from steam
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -11,14 +11,14 @@ goto oStart
 document.querySelectorAll('a').forEach(link =>{if (link.href.includes("steamcommunity.com/sharedfiles/filedetails/?id=")){fetch(link.href).then(response => {return response.text();}).then(html =>{let parser = new DOMParser();let doc = parser.parseFromString(html, 'text/html');doc.querySelectorAll('a').forEach(link =>{if (link.href.includes("images.steamusercontent.com/ugc/")){console.log(link.href.slice(0, link.href.indexOf('?')));};});}).catch(error =>{console.error('error cal site:', error);});}});
 
 4. in your browser dev log you see a lot of image links so hit the right mouse button and save the log as links.log inside the mdhSteamScreenshotsDownloader folder
-5. open the links.log file with an texteditor and clear the file so that only a list of valid links is in the links.log file
+5. (NO LONGER NEEDED) open the links.log file with an texteditor and clear the file so that only a list of valid links is in the links.log file
 6. run the mdhSteamScreenshotsDownloader.bat and it will start downloading all screenshots of the links.log file
 
 
 :oStart
-echo -----------------------------------------------------------------------------------------------------------------------
-echo mdhSteamScreenshotsDownloader by Moerderhoschi - github: https://github.com/Moerderhoschi/mdhSteamScreenshotsDownloader
-echo -----------------------------------------------------------------------------------------------------------------------
+echo ------------------------------------------------------------------------------------------------------------------------------------
+echo mdhSteamScreenshotsDownloader(by Moerderhoschi) v2025-05-26 - github: https://github.com/Moerderhoschi/mdhSteamScreenshotsDownloader
+echo ------------------------------------------------------------------------------------------------------------------------------------
 echo Option 1: download all screenshots from links.log file
 echo Option 2: download all screenshots from links.log file and make the timestamp better readable
 echo Option 3: download all screenshots from links.log file and add game name to screenshots if available
@@ -28,14 +28,15 @@ set _n=1
 set /p _n=
 
 mkdir screenshots
-powershell -c "gc \"links.log\" | ForEach-Object {  $a=$_ ; $b=wget $a ; $c=$b.headers[\"Content-Disposition\"] ; $d = $c -replace '.*filename=\"(.*)\";', '$1' ; wget $a -o screenshots\$d }"
+::powershell -c "gc \"links.log\" | ForEach-Object {  $a=$_.SubString($_.IndexOf(\"https://images.steamusercontent.com/ugc/\")) ; $b=wget $a ; $c=$b.headers[\"Content-Disposition\"] ; $d = $c -replace '.*filename=\"(.*)\";', '$1' ; wget $a -o screenshots\$d }"
+powershell -c "gc \"links.log\" | ForEach-Object {  $a=$_.SubString($_.IndexOf(\"https://images.steamusercontent.com/ugc/\")) ; $i=$a.indexOf(\"/\",40) ; $i=$a.indexOf(\"/\",$i+1) ; $a=$a.subString(0, $i+1) ; $b=wget $a ; $c=$b.headers[\"Content-Disposition\"] ; $d = $c -replace '.*filename=\"(.*)\";', '$1' ; wget $a -o screenshots\$d }"
 if %_n% == 1 GOTO o1
 if %_n% == 3 GOTO o3
 
 cd screenshots
 mkdir temp
 @echo off & Setlocal EnableDelayedExpansion
-for %%g in (*.jpg) do (
+for %%g in (*_screenshots_20*.jpg) do (
 set _g=%%g
 
 	for /f "tokens=1,2,3,4 delims=_" %%a in ("!_g!") do (
@@ -76,11 +77,13 @@ powershell -c "$n='Arma Cold War Assault 65790 ';$o='65790_screenshots_';gci -Re
 powershell -c "$n='Arma Reforger 1874880 ';$o='1874880_screenshots_';gci -Recurse | Where-Object{$_.Name -match $o} | foreach{$cn=$_.Name;$nn=$cn -replace $o, $n ; echo ($cn + ' -- RENAMED TO -- ' + $nn) ; ren $_.fullname $nn}"
 powershell -c "$n='Arma Reforger Experimental 1890860 ';$o='1890860_screenshots_';gci -Recurse | Where-Object{$_.Name -match $o} | foreach{$cn=$_.Name;$nn=$cn -replace $o, $n ; echo ($cn + ' -- RENAMED TO -- ' + $nn) ; ren $_.fullname $nn}"
 powershell -c "$n='Arma Reforger Tools 1874910 ';$o='1874910_screenshots_';gci -Recurse | Where-Object{$_.Name -match $o} | foreach{$cn=$_.Name;$nn=$cn -replace $o, $n ; echo ($cn + ' -- RENAMED TO -- ' + $nn) ; ren $_.fullname $nn}"
-powershell -c "$n='Grand Theft Auto V 271590 ';$o='9974983548387459072_screenshots_';gci -Recurse | Where-Object{$_.Name -match $o} | foreach{$cn=$_.Name;$nn=$cn -replace $o, $n ; echo ($cn + ' -- RENAMED TO -- ' + $nn) ; ren $_.fullname $nn}"
-powershell -c "$n='Grand Theft Auto V 271590 ';$o='271590_screenshots_';gci -Recurse | Where-Object{$_.Name -match $o} | foreach{$cn=$_.Name;$nn=$cn -replace $o, $n ; echo ($cn + ' -- RENAMED TO -- ' + $nn) ; ren $_.fullname $nn}"
+powershell -c "$n='Grand Theft Auto V 3240220 ';$o='9974983548387459072_screenshots_';gci -Recurse | Where-Object{$_.Name -match $o} | foreach{$cn=$_.Name;$nn=$cn -replace $o, $n ; echo ($cn + ' -- RENAMED TO -- ' + $nn) ; ren $_.fullname $nn}"
+powershell -c "$n='Grand Theft Auto V 3240220 ';$o='9690426696418721792_screenshots_';gci -Recurse | Where-Object{$_.Name -match $o} | foreach{$cn=$_.Name;$nn=$cn -replace $o, $n ; echo ($cn + ' -- RENAMED TO -- ' + $nn) ; ren $_.fullname $nn}"
+powershell -c "$n='Grand Theft Auto V 3240220 ';$o='271590_screenshots_';gci -Recurse | Where-Object{$_.Name -match $o} | foreach{$cn=$_.Name;$nn=$cn -replace $o, $n ; echo ($cn + ' -- RENAMED TO -- ' + $nn) ; ren $_.fullname $nn}"
 powershell -c "$n='Grand Theft Auto V 3240220 ';$o='3240220_screenshots_';gci -Recurse | Where-Object{$_.Name -match $o} | foreach{$cn=$_.Name;$nn=$cn -replace $o, $n ; echo ($cn + ' -- RENAMED TO -- ' + $nn) ; ren $_.fullname $nn}"
 powershell -c "$n='Ready or Not 1144200 ';$o='1144200_screenshots_';gci -Recurse | Where-Object{$_.Name -match $o} | foreach{$cn=$_.Name;$nn=$cn -replace $o, $n ; echo ($cn + ' -- RENAMED TO -- ' + $nn) ; ren $_.fullname $nn}"
 powershell -c "$n='UBOAT 494840 ';$o='494840_screenshots_';gci -Recurse | Where-Object{$_.Name -match $o} | foreach{$cn=$_.Name;$nn=$cn -replace $o, $n ; echo ($cn + ' -- RENAMED TO -- ' + $nn) ; ren $_.fullname $nn}"
+
 exit
 
 :o1
